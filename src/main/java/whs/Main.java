26ee863d98902ae9269f8e5445adb45aceffd8a8
wps-dpetty <dpetty@winchesterps.org>
2018-06-02 22:06:26 -0400

    Initial version of project files.

---
+++ src/main/java/whs/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Main.java
+ *
+ * @author 2017-2018 APCS
+ * @author ADD @author TAG FOR EVERYONE WHO CONTRIBUTED TO THIS FILE
+ * @author David C. Petty <dpetty@winchesterps.org>
+ */
+package whs;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    /**
+     * Whs main method.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("# Main");
+    }
+}
---
+++ src/test/java/whs/MainTest.java
@@ -0,0 +1,35 @@
+/*
+ * MainTest.java
+ *
+ * @author 2017-2018 APCS
+ * @author ADD @author TAG FOR EVERYONE WHO CONTRIBUTED TO THIS FILE
+ * @author David C. Petty <dpetty@winchesterps.org>
+ */
+package whs;
+
+import org.junit.*;
+
+/**
+ * Unit test for {@link Main}.
+ */
+public class MainTest {
+    /**
+     * Set up before every test.
+     */
+    @Before
+    public void setUp() {
+    }
+    /**
+     * Tear down after every test.
+     */
+    @After
+    public void tearDown() {
+    }
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        Assert.assertTrue( true );
+    }
+}
