8187d7eea5bfbae52b892774c0cbd58c9e032c87
wps-dpetty <dpetty@winchesterps.org>
2018-06-12 01:17:31 -0400

    Major restructuring of Main.java to use a GridBagLayout, to minimize fields when they were not necessary, and to use the new Button, Grid, and Images classes. Reformatted Main.java. Commented out some of the unnecessary imports.

--- src/main/java/minesweeper/Main.java
+++ src/main/java/minesweeper/Main.java
@@ -5,10 +5,11 @@ package minesweeper;
 
 import java.awt.*;
 import java.awt.event.*;
+//import java.awt.image.*;
 import javax.swing.*;
-import javax.swing.border.*;
-import java.text.*;
-import java.util.*;
+//import javax.swing.border.*;
+//import java.text.*;
+//import java.util.*;
 import org.apache.logging.log4j.*;
 
 /**
@@ -22,69 +23,104 @@ public class Main extends JFrame {
     /** log4j {@link Logger}. */
     private static Logger logger = LogManager.getLogger(Minesweeper.SHORT);
 
-    static int row = 16, col = 30;
-    static int mines = 99;
+    private static int row = 16, col = 30;
+    private static int mines = 99;
 
-    static JPanel topPanel = new JPanel();
+    private static Stopwatch sw;
+    private static JLabel timerLabel;
+    private static Grid grid;
 
-    static JLabel labelTimer = new JLabel("00:00");
-    static javax.swing.Timer mainTimer;
-    static Stopwatch sw;
-    static JLabel mineCount = new JLabel("0" + mines);
-    static ImageIcon smileicon = new ImageIcon("smile.gif");
-    static JButton smile = new JButton(smileicon);
+    // RED_FLAG: these fields are not used
+    // private static final SimpleDateFormat sdfTimer  = new SimpleDateFormat("mm:ss");
+    // private static int currentSecond;
+    // private static Calendar calendar;
 
-    private static final SimpleDateFormat sdfTimer  = new SimpleDateFormat("mm:ss");
-    private static int currentSecond;
-    private static Calendar calendar;
-
-    GridLayout experimentLayout = new GridLayout(row, col);
+    //GridLayout experimentLayout = new GridLayout(row, col);
 
     public Main(String name) {
         super(name);
+        sw = new Stopwatch();
         logger.info("{}: {}", getClass(), name);
-        setResizable(false);
     }
 
-    public void createGrid(final Container pane) {
-        final JPanel addTiles = new JPanel();
-        addTiles.setLayout(experimentLayout);
-        
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {   
-                Tile tile = new Tile(i, j);
-                tile.setPreferredSize(new Dimension(20, 20));
-                addTiles.add(tile);
-            }
-        }
-
-        pane.add(new JSeparator(), BorderLayout.CENTER);
-        pane.add(addTiles, BorderLayout.SOUTH);
-    }
+    public static Grid getGrid() { return grid; }
 
     private static void createAndShowGUI() {
         //Create and set up the window.
         Main frame = new Main("Minesweeper");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        labelTimer.setFont(new Font("Verdana", 1, 20));
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        // Display mineCount.
+        JLabel mineCount = new JLabel("0" + mines, SwingConstants.CENTER);
         mineCount.setFont(new Font("Verdana", 1, 20));
-        smile.setPreferredSize(new Dimension(24, 24)); 
+        c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.ipadx = 10;
+        c.weightx = 0.5;
+        frame.getContentPane().add(mineCount, c);
+  
+        // Display smile.
+        Image image = Images.getImage("/images/felix.gif");
+        ImageIcon smileicon = new ImageIcon(image);
+        JButton smile = new JButton(smileicon);
+        //smile.setPreferredSize(new Dimension(35, 35));
+        c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.PAGE_START;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.NONE;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.ipadx = 10;
+        c.weightx = 0.5;
+        frame.getContentPane().add(smile, c);
 
-        topPanel.setLayout(new BorderLayout((col * 10), 0));
-        topPanel.add(mineCount, BorderLayout.WEST);
-        topPanel.add(smile);
-        topPanel.add(labelTimer, BorderLayout.EAST);
+        // Display timerLabel.
+        timerLabel = new JLabel("00:00", SwingConstants.CENTER);
+        timerLabel.setFont(new Font("Verdana", 1, 20));
+        c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.FIRST_LINE_END;
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridy = 0;
+        c.ipadx = 10;
+        c.weightx = 0.5;
+        frame.getContentPane().add(timerLabel, c);
 
-        topPanel.setBorder(BorderFactory.createCompoundBorder(new EmptyBorder(10, 10, 10, 10), new EmptyBorder(0, 0, 0, 0)));
+        // Display separator.
+        JSeparator separator = new JSeparator();
+        c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.CENTER;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.ipadx = 0;
+        c.gridwidth = 3;
+        c.weightx = 0.5;
+        frame.getContentPane().add(separator, c);  
 
-        sw = new Stopwatch();
+        // Display grid.
+        grid = new Grid(row, col);
+        c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.SOUTH;
+        c.fill = GridBagConstraints.NONE;
+        c.gridx = 0;
+        c.gridy = 2;
+        c.ipadx = 0;
+        c.gridwidth = 3;
+        c.weightx = 0.5;
+        frame.getContentPane().add(grid, c);  
 
-        frame.getContentPane().add(topPanel, BorderLayout.NORTH);  
-        //Set up the content pane.
-        frame.createGrid(frame.getContentPane());       
         //Display the window.
         frame.pack();
+        frame.setMinimumSize(frame.getSize());  // layout will not get smaller
         frame.setVisible(true);
     }
 
@@ -93,9 +129,9 @@ public class Main extends JFrame {
         if (resetTimer || (!sw.isStopWatchRunning())) 
         {
             sw.startStopWatch();
-            mainTimer = new javax.swing.Timer(100, new ActionListener(){
+            javax.swing.Timer mainTimer = new javax.swing.Timer(100, new ActionListener(){
                 public void actionPerformed( ActionEvent e ) {
-                    labelTimer.setText(sw.getFormattedElapsedTime());
+                    timerLabel.setText(sw.getFormattedElapsedTime());
                 }
             });
             mainTimer.start();
