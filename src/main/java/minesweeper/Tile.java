4530d2c848a3fbe6b1dd04d45d6f4741fdb89f82
KKhaghani <39408634+KKhaghani@users.noreply.github.com>
2018-06-13 08:29:45 -0400

    Updated Tile to have the tileValue field (for the number on the tiles)
    TileValue should, ideally, be programmed in main: while generating the Tile[][] array, there are a few ways that either B or C block could get tileValue to increment to its proper value. I can explain in class or via discord, or spearhead the code myself.

--- src/main/java/minesweeper/Tile.java
+++ src/main/java/minesweeper/Tile.java
@@ -11,6 +11,7 @@ import org.apache.logging.log4j.*;
  *
  * @author 2017-2018 APCS
  * @author ADD @author TAG FOR EVERYONE WHO CONTRIBUTED TO THIS FILE
+ * @author Kevin Khaghani // 1907024@wpsstudent.com
  * @author David C. Petty // https://github.com/wps-dpetty
  */
 public class Tile extends JButton implements ActionListener {
@@ -20,12 +21,14 @@ public class Tile extends JButton implements ActionListener {
     private int row;
     private int column;
     private boolean isMine;
+    private int tileValue;
     
     public Tile(int r, int c) {
         logger.info(this);
         addActionListener(this);
         row = r;
         column = c;
+        tileValue = 0;
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -39,5 +42,26 @@ public class Tile extends JButton implements ActionListener {
 
     public int getCol() {
         return column;
-    } 
+    }
+    
+    /**
+     * Method for Main to use to read the value of the tile and, in turn, display it on the eventual board
+     * @return the value on the tile
+     */
+    public int getTileValue() { //for now, if it's 0, no bombs around, do NOT display 0- display a blank tile.
+  	  return tileValue;
+    }
+    /**
+     * Allows Main (or wherever the board is initialized) to modify the value based on nearby bombs
+     * @param a = the value to which tileValue will be set
+     */
+    public void setTileValue(int a) {
+  	  tileValue = a;
+    }
+    /**
+     * Another way to modify the tileValue, incrementing by one- use this instead of the other if it suits the board-init code better
+     */
+    public void incrementTile() {
+  	  tileValue++;
+    }
 }
